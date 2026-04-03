871f59fafe8889b828df89269e19f6a9a2f114f1
Mawais-10 <fa23-bcs-061@cuiatk.edu.pk>
2026-04-03 21:22:32 +0500

    revvising Useeffect with examples

--- React-concepts/src/App.tsx
+++ React-concepts/src/App.tsx
@@ -4,9 +4,11 @@
 // import Profile from "./Components/Profile";
 // import RenderingList from "./RenderingList";
 // import Statee from "./State";
-
-import Updatingfromvalues from "./Components/Revising a Usestate Hook/Updatingfromvalues";
-import Initial_function from "./Passing the initializer function/Initial_function";
+import ChatRoom from "./Components/Useeffect/chat";
+// import { Conditionalrendering } from "./Components/Conditional-rendering";
+// import Conn_rend from "./Components/Conditional_rendering/Conn_rend";
+// import Updatingfromvalues from "./Components/Revising a Usestate Hook/Updatingfromvalues";
+// import Initial_function from "./Passing the initializer function/Initial_function";
 
 // import Board from "./Components/Tic-Tac-Toe/Board";
 // import Hook from "./Components/Revising a Usestate Hook/UsestateHook";
@@ -22,17 +24,17 @@ function App() {
     <div>
       {/* <Board/> */}
       {/* <Hook/> */}
-      <Updatingfromvalues />
-    
+      {/* <Updatingfromvalues /> */}
+      {/* <Conn_rend /> */}
       {/* <h1>All components will be rendered here</h1>
 //     <Buttons value={count} onClick={handleClick}/>
 //     <Buttons value={count} onClick={handleClick}/> */}
       // {/* <Profile /> */}
       // {/* <Conditionalrendering /> */}
-      
       {/* <RenderingList/>
 //     <Statee/> */}
-      <Initial_function />
+      {/* <Initial_function /> */}
+      <ChatRoom />
     </div>
   );
 }
---
+++ React-concepts/src/Components/Conditional_rendering/Conn_rend.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Item({ name, isPacked }: { name: string; isPacked: boolean }) {
+  return <li className="item">{name}</li>;
+}
+
+export default function Conn_rend() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={true} name="Helmet with a golden leaf" />
+        <Item isPacked={false} name="Photo of Tam" />
+      </ul>
+    </section>
+  );
+}
